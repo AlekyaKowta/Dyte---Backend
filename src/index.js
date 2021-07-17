0281b4a7e04680ebe0ddcb6be6b501c2e68d8ccd
AlekyaKowta <akowta@gmail.com>
2021-07-17 20:40:38 +0530

    concurrency

--- src/index.js
+++ src/index.js
@@ -98,6 +98,30 @@ app.delete("/deletewUID/:id", async (req, res) => {
   }
 });
 
+// //Concurrency Code
+// const pAll = async (queue, concurrency) => {
+//   let index = 0;
+//   const results = [];
+
+//   // Run a pseudo-thread
+//   const execThread = async () => {
+//     while (index < queue.length) {
+//       const curIndex = index++;
+//       // Use of `curIndex` is important because `index` may change after await is resolved
+//       results[curIndex] = await queue[curIndex]();
+//     }
+//   };
+
+//   // Start threads
+//   const threads = [];
+//   for (let thread = 0; thread < concurrency; thread++) {
+//     threads.push(execThread());
+//   }
+//   await Promise.all(threads);
+//   return results;
+// };
+
+
 app.get("/trigger/:ipAddress", async (req, res) => {
   try {
     const urls = await Register.find({});
@@ -134,6 +158,14 @@ app.get("/trigger/:ipAddress", async (req, res) => {
     getPosts(0).then(() => {
       console.log("done");
     });
+
+    // const test = async () => {
+    //   const res = await pAll(
+    //     targetURLs.map(url => () => makeHTTPRequest(url)),
+    //     5
+    //   );
+    //   console.log(res);
+    // };
 
     res.send("Done");
   } catch (e) {
